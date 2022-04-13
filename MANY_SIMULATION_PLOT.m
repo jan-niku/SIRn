d0e67bcdbb681bfecde28e690a0ee71bcec446ee
forjx <forjx@192.168.0.97>
2022-04-13 10:08:08 -0600

    added barebones batch simulation cell population for future plotting

---
+++ MANY_SIMULATION_PLOT.m
@@ -0,0 +1,22 @@
+% Experimental code for plotting many simulations at once
+
+%% Parameters
+% Global 
+SIRDIR = uigetdir;
+SIRDIR = SIRDIR + "/";
+
+% Grabbing
+sim_first = 1;
+sim_last = 20;
+sim_step = 1;
+compartments = [1, ... % infected
+                0, ... % new infected
+                0, ... % recovered
+                0];    % cumulative infected
+
+% Generate the simulation and compartments array
+simulation_numbers = sim_first:sim_step:sim_last;
+compartments = find(compartments); % grab idx of nonzero
+
+Series = sim_grabber(simulation_numbers, compartments, SIRDIR);
+
---
+++ sim_grabber.m
@@ -0,0 +1,27 @@
+%% This function will grab time series from simulations
+% it expects an array to be passed of the numbers you want
+% and an array of the compartments you want output
+% it returns a cell of outputs
+
+function Series = sim_grabber(simulation_numbers, compartments, SIRDIR)
+
+    % build cell
+    qty_simulations = length(simulation_numbers);
+    qty_compartments = length(compartments);
+    Series = cell(qty_compartments,1,qty_simulations);
+
+    % iterate over files and populate
+    for simnum=1:qty_simulations
+        numpadded = sprintf('%04d',simulation_numbers(simnum));
+        FILENAME = "sim"+numpadded+".txt";
+        FILEPATH = SIRDIR+FILENAME;
+
+        simulation = readmatrix(FILEPATH);
+
+        for srs=1:qty_compartments
+            % series row needs to be indexed by consecutive ints
+            % but we do not have to pull consecutive series from simulation
+            Series{srs,1,simnum} = simulation(compartments(srs),:);
+        end
+    end
+end
